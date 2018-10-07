222116476539f64a9e31d680e5f9aa97692a1d09
axtora <axtora@naver.com>
2018-10-07 17:19:28 +0900

    beta

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import Movie from './Movie';
 class App extends Component {
   state = {}
   componentDidMount() {
-    console.log(fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating'))
+    this._getMovies()
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      console.log(movie)
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=like_count')
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(err => console.error(err))
   }
 
   render() {
