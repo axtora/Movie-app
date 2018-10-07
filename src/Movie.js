e28c7988c5b7f265c9e6c234d742fc2fab61889f
axtora <axtora@naver.com>
2018-10-07 15:19:05 +0900

    beta

--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 
-class Movie extends Component {
-    render() {
-        console.log(this.props)
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h3>{this.props.title}</h3>
-            </div>
-        )
-    }
+function Movie({title, poster}) {
+    return (
+    <div>
+        <MoviePoster poster={poster}/>
+        <h3>{title}</h3>
+    </div>        
+    )
 }
 
-class MoviePoster extends Component {
-    render() {
-        return (
-            <img src={this.props.poster}/>
-        )
-    }
+function MoviePoster ({poster}) {
+    return (
+        <img src={poster} alt='Movie poster' />
+    )
 }
+
+// class Movie extends Component {
+//     render() {
+//         console.log(this.props)
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster}/>
+//                 <h3>{this.props.title}</h3>
+//             </div>
+//         )
+//     }
+// }
+
+// class MoviePoster extends Component {
+//     render() {
+//         return (
+//             <img src={this.props.poster} alt='movie poster' />
+//         )
+//     }
+// }
+
 
 
 export default Movie;
