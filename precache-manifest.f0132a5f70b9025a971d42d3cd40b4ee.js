e879735d5dce033a8ae0ef5712e06c03483626d8
axtora <axtora@naver.com>
2018-10-07 18:57:00 +0900

    Updates

---
+++ precache-manifest.f0132a5f70b9025a971d42d3cd40b4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b860a52aeef2ba208ba7",
+    "url": "/Movie-app/static/js/runtime~main.b860a52a.js"
+  },
+  {
+    "revision": "9ae7deb563b974535f0d",
+    "url": "/Movie-app/static/js/main.9ae7deb5.chunk.js"
+  },
+  {
+    "revision": "560bf4b348b065a6bf3a",
+    "url": "/Movie-app/static/js/1.560bf4b3.chunk.js"
+  },
+  {
+    "revision": "9ae7deb563b974535f0d",
+    "url": "/Movie-app/static/css/main.b06d3407.chunk.css"
+  },
+  {
+    "revision": "9b9959b454d1bee7ac5dc2058d510faa",
+    "url": "/Movie-app/index.html"
+  }
+];
